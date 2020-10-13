43e1a652cc068fff225560fa23c36393ee6449f3
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-13 19:50:15 +0530

    checked api using postman

--- api.py
+++ api.py
@@ -3,3 +3,10 @@ from flask_restful import  Resource,Api
 from main import run_main
 import json
 import config as cfg
+
+app = Flask(__name__)
+api = Api(app)
+
+
+if __name__ =='__main__':
+    app.run(debug = True)   
