58f70bb4f89bdcdc464fa604e59b84e6f25a2159
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-13 19:53:44 +0530

    get route for fetch runmain func() including flag for sending mail if 1

--- api.py
+++ api.py
@@ -7,11 +7,58 @@ import config as cfg
 app = Flask(__name__)
 api = Api(app)
 
+
 ######################
 ##config
 ######################
 configuration_counts = (cfg.configuration_count)
 flag = (cfg.flag)
+
+
+######################
+##get run_main()
+######################
+class GetAll(Resource):
+    def get(self,flag):
+        try:
+            if flag == flag:
+                run = run_main(configuration_counts,flag)
+                return Response(
+                response = json.dumps(run,default=str), 
+                status=200,
+                mimetype="application/json"
+                ) 
+            elif flag != flag:
+                run = run_main(configuration_counts,flag)
+                return Response(
+                response = json.dumps(run,default=str), 
+                status=200,
+                mimetype="application/json"
+                )
+        except Exception as e:
+            return Response(
+            response = json.dumps(
+                {"message":"Check Your URL Please!!"},default=str), 
+                status=404,
+                mimetype="application/json"
+                ) 
+
+
+
+
+
+
+
+
+
+
+
+######################
+##api call
+######################
+api.add_resource(GetAll,"/configuration/<int:flag>/")
+
+
 
 
 if __name__ =='__main__':
