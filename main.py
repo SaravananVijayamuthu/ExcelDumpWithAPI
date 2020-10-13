4829d9c2be9191e4c767aa2fe12c3dabb3623391
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-13 19:33:44 +0530

    imported needed packages main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from pymongo import MongoClient
+from shutil import copyfile
+import pandas as pd
+import datetime
+import csv
+import schedule
+import string
+import yagmail
+import xlsxwriter
+import os
+import config as cfg
