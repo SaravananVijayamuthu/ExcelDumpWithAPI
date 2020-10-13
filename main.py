30f831ee0e2637d84ce218532d9bbc06948eaf2c
Saravanan raghul <saravananraghul1820@gmail.com>
2020-10-13 19:37:32 +0530

    created func. for counting all docs from config.

--- main.py
+++ main.py
@@ -16,4 +16,13 @@ import config as cfg
 ######################
 client=MongoClient(cfg.uri)
 db_name=cfg.dbname
-db=client[db_name]   
+db=client[db_name]
+
+######################
+##Count all docs
+######################
+def docs_count():
+    for i in range(len(cfg.configuration_count)):
+        total_documents=db[cfg.configuration_count["stats_for_{}".format(i+1)]["collection_name"]].count_documents({})
+        total_documents_list.append(total_documents)
+    return total_documents_list
